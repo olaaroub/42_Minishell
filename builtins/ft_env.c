ae987d5d3500118f4fa3a86ca220b094ae108503
Hamza Talhaoui <hatalhao@c4r2p10.1337.ma>
2024-11-27 19:21:13 +0100

    ...

--- builtins/ft_env.c
+++ builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: hatalhao <hatalhao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 09:26:50 by hatalhao          #+#    #+#             */
-/*   Updated: 2024/11/17 01:47:47 by hatalhao         ###   ########.fr       */
+/*   Updated: 2024/11/27 19:15:51 by hatalhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	ft_env(void)
 	iter = g_data.env_list;
 	while (iter)
 	{
-		printf("%s=%s\n", iter->name, iter->value);
+		if (iter->value)
+			printf("%s=%s\n", iter->name, iter->value);
 		iter = iter->next;
 	}
 	g_data.ret_value = 0;
